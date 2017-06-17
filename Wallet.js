4dc6bd1f90ecc701230320133bcccb6b3cc2a694
xeleniumz <xeleniumz@users.noreply.github.com>
2017-06-17 19:23:42 +0700

    Update and rename .gitignore to Wallet.js

---
+++ Wallet.js
@@ -0,0 +1,102 @@
+/**
+ * @version 1.0
+ * @author Xeleniumz Fx
+ * @type require  ES5 ,ES6
+ * @module convert from https://github.com/exzajung/class.truewallet.php
+ */
+
+import requestPromise from 'request-promise-native'
+import moment from 'moment'
+
+
+const api_signin = "https://api-ewm.truemoney.com/api/v1/signin?&";
+const api_profile = "https://api-ewm.truemoney.com/api/v1/profile/";
+const api_topup = "https://api-ewm.truemoney.com/api/api/v1/topup/mobile/";
+const device_os = "android";
+const device_id = "d520d0d12d0d48cb89394905168c6ed5";
+const device_type = "CPH1611";
+const device_version = "6.0.1";
+const app_name = "wallet";
+const app_version = "2.9.14";
+const deviceToken = "fUUbZJ9nwBk:APA91bHHgBBHhP9rqBEon_BtUNz3rLHQ-sYXnezA10PRSWQTwFpMvC9QiFzh-CqPsbWEd6x409ATC5RVsHAfk_-14cSqVdGzhn8iX2K_DiNHvpYfMMIzvFx_YWpYj5OaEzMyIPh3mgtx";
+const mobileTracking = "dJyFzn\/GIq7lrjv2RCsZbphpp0L\/W2+PsOTtOpg352mgWrt4XAEAAA==";
+
+
+
+const Wallet = {
+
+    async getToken(req, res) {
+        let data = {
+            "username": req.body.username,
+            "password": req.body.password,
+            "type": login_type,
+            "deviceToken": deviceToken,
+            "mobileTracking": mobileTracking,
+        }
+
+        let uri = api_signin + 'device_os=' + device_os + '&device_id=' +
+            +device_id + '&device_type=' + device_type + '&device_version=' + device_version + '&app_name=' + app_name + '&app_version=' + app_version
+
+        let headers = {
+            "Host": "api-ewm.truemoney.com",
+            "Content-Type": "application/json"
+        }
+
+
+        return await wallet_request(headers, uri, data)
+
+
+
+    },
+
+    async Profile(req, res, token) {
+        let url = api_profile + token + '?&device_os=android&device_id=' + device_id + '&device_type=' + device_type + '&device_version=' +
+            device_version + '&app_name=' + app_name + '&app_version=' + app_version
+        let headers = {
+            "Host": "api-ewm.truemoney.com"
+        }
+        return await wallet_request(headers, uri)
+    },
+
+    async Topup(req, res, token) {
+        let url = api_topup + moment().tz("Asia/Bangkok").format('YYYY-MM-DD HH:mm:ss') + "/" + token + "/cashcard/" + req.body.cashcard;
+        let headers = {
+            "Host": "api-ewm.truemoney.com"
+        }
+        return wallet_request($url, true, $header);
+    },
+
+    async wallet_request(headers, uri, data) {
+        if (typeof data === 'undefined') {
+            var options = {
+                header: headers,
+                method: 'POST',
+                uri: uri,
+                json: true
+            }
+        } else {
+            var options = {
+                header: headers,
+                method: 'POST',
+                uri: uri,
+                body: data,
+                json: true
+            }
+        }
+
+
+        return requestPromise(options)
+            .then(function(parsedBody) {
+                return parsedBody
+            })
+            .catch(function(err) {
+                return err
+            });
+    },
+
+
+
+}
+
+
+export default Wallet
